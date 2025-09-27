edc1a34da52db78561aa0ccf8f163c41585041bc
Enderic <133310359+Enderic@users.noreply.github.com>
2025-09-27 00:07:46 -0500

    basic counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::{button, text, Column, column}, Element};
+
+fn main() -> iced::Result {
+    iced::run("A cool counter", Counter::update, Counter::view)
 }
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+
+impl Counter {
+    pub fn view(&'_ self) -> Column<'_, Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+    Decrement,
+}
+
