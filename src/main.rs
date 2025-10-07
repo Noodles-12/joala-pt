b861ea9a018af4d7902adf4b1437ed01b0b99d7f
Noodles <mahiyan21@gmail.com>
2025-10-07 11:15:30 -0500

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,9 @@
 mod widget;
 
-use iced::{widget::{button, text, Column, column}};
+use widget::Add;
 
 
 fn main() -> iced::Result {
-    iced::run("A cool counter", Counter::update, Counter::view)
-}
-
-#[derive(Default)]
-struct Counter {
-    value: i32,
-}
-
-impl Counter {
-    pub fn view(&self) -> Column<'_, Message> {
-        column![
-            button("+").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("-").on_press(Message::Decrement),
-        ]
-    }
-
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    Increment,
-    Decrement,
+    iced::run("A cool counter", Add::update, Add::view)
 }
 
--- src/widget.rs
+++ src/widget.rs
@@ -1,20 +1,61 @@
 use iced::widget::{container, Container};
+use iced::Element;
+use iced::{widget::{button, text, Column, column}};
 
-pub struct Add {}
+#[derive(Default)]
+pub struct Add {
+    counter: Counter,
+}
 
+#[derive(Debug, Clone, Copy)]
 pub enum AddMessage {
     Percentage(f32)
 }
 
 impl Add {
     pub fn view(&self) -> Container<'_, AddMessage> {
-        container("Text")
+        container(
+            self.counter.view().
+                
+        )
             .padding(20)
             .style(container::rounded_box)
             .into()
     }
 
-    pub fn update() {
-        
+    pub fn update(&mut self, message: AddMessage) {
+
+    } 
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+
+impl Counter {
+    pub fn view(&self) -> Column<'_, Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("-").on_press(Message::Decrement),
+        ]
     }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+    Decrement,
 }
