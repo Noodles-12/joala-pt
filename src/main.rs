f5e136d51c8bab987c499e5171412f0897604306
Enderic <133310359+Enderic@users.noreply.github.com>
2025-10-12 18:04:00 -0500

    git

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod widget;
 
 use widget::Add;
-
+use widget::Grid;
 
 fn main() -> iced::Result {
     iced::run("A cool counter", Add::update, Add::view)
--- src/widget.rs
+++ src/widget.rs
@@ -1,6 +1,9 @@
-use iced::widget::{container, Container};
-use iced::Element;
-use iced::{widget::{button, text, column}};
+use iced::alignment::{Horizontal, Vertical};
+use iced::widget::{container, scrollable, Container};
+use iced::{Element, Length};
+use iced::{widget::{button, text, column, row}};
+
+static BOX_SIZE: u32 = 160;
 
 #[derive(Default)]
 pub struct Add {
@@ -10,24 +13,40 @@ pub struct Add {
 #[derive(Debug, Clone, Copy)]
 pub enum AddMessage {
     Counter(CounterMsg),
+    Add,
 }
 
 impl Add {
     pub fn view(&self) -> Container<'_, AddMessage> {
+        println!("Testing");
         container(
-            self.counter.view().map(move |msg| { AddMessage::Counter(msg)})
+            //self.counter.view().map(move |msg| { AddMessage::Counter(msg)})
+            button("+")
+                .on_press(AddMessage::Add)
+                .width(Length::Fixed(BOX_SIZE as f32))
+                .height(Length::Fixed(BOX_SIZE as f32)),
         )
             .padding(20)
             .style(container::rounded_box)
+            .width(Length::Fixed(BOX_SIZE as f32))
+            .height(Length::Fixed(BOX_SIZE as f32))
+            .align_x(Horizontal::Center)
+            .align_y(Vertical::Center)
     }
 
     pub fn update(&mut self, message: AddMessage) {
         match message {
             AddMessage::Counter(msg) => {
                 self.counter.update(msg);
-            }
+            },
+            AddMessage::Add => println!("Add Button Pressed")
         }
     } 
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum GridMsg {
+
 }
 
 #[derive(Default)]
@@ -37,6 +56,7 @@ struct Counter {
 
 impl Counter {
     pub fn view(&self) -> Element<'_, CounterMsg> {
+        println!("Testing");
         column![
             button("+").on_press(CounterMsg::Increment),
             text(self.value).size(50),
@@ -60,4 +80,19 @@ impl Counter {
 pub enum CounterMsg {
     Increment,
     Decrement,
+}
+
+#[derive(Default)]
+pub struct Grid {
+    
+}
+
+impl Grid {
+    pub fn view(&self) {
+        
+    }
+
+    pub fn update(&mut self, msg: GridMsg) {
+
+    }
 }
